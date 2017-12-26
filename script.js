1df5273c3b2e85b32bbc4ec5a873006c429ca68b
New Computer <admin@Sarahs-MacBook-Air.local>
2017-12-26 14:41:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// debugger;
+var beers = [];
+
+function addBeer(name, category, rate) {
+    var beer = {
+        name: name,
+        category: category,
+        rate: rate
+    }
+    beers.push(beer);
+}
+
+function renderBeers() {
+    $(".beers").find("li").remove();
+    for (var i = 0; i < beers.length; i++) {
+        var beer = beers[i];
+        $(".beers-list").append("<li>" + beer.name + " " + beer.category + "</li>");
+    }
+}
+
+$(".post-beer").click(function () {
+    addBeer($(".beer-input").val(), $(".category-input").val())
+    renderBeers(); 
+    console.log("sucess");
+})
+
+
+// $(".sort-btn").click(function () {
+//     // addbeer($beer - inp)
+
+// }
